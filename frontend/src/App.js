5d2e8abb88c60670e6c513161f81702e9c47dbdb
dkblackley <dkblackley@dundee.ac.uk>
2021-12-17 00:00:23 +0000

    compartmentised the logo into a new component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import { LoginForum } from './LoginForum'
+import Logo from './Logo'
 
 function App() {
   return (
     <>
     <div className="App" className="App-background">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to scream.
-        </p>
-        </header>
-        <body className="App-body">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Logo />
+      <body className="App-body">
         <LoginForum />
       </body>
     </div>
--- frontend/src/LoginForum.js
+++ frontend/src/LoginForum.js
@@ -4,22 +4,50 @@ export class LoginForum extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = 2;
 
+    this.state = {
+      username : '',
+      password : ''
+    }
+
+    this.updateName = this.updateName.bind(this);
+    this.updatePass = this.updatePass.bind(this);
     this.loginRequest = this.loginRequest.bind(this);
   }
 
-  loginRequest() {
-    console.log(this.state);
-    this.state = this.state + 1;
+  updateName(input) {
+    this.setState({username : input.target.value})
+  }
+
+  updatePass(input) {
+    this.setState({password : input.target.value})
+  }
+
+  loginRequest(username, password) {
+    console.log(this.state.username);
+    console.log(this.state.password);
   }
 
   render () {
     return (
       <div className="App-body">
-        <input type="text" name ="Username" placeholder="Username" />
-        <input type="text" name ="Password" placeholder="Password" />
-        <button onClick={this.loginRequest}>Hello</button>
+        <p>
+          Edit <code>src/App.js</code> and save to scream.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <input type="text" name ="Username" placeholder="Username"
+          onChange={this.updateName} />
+        <input type="text" name ="Password" placeholder="Password"
+          onChange={this.updatePass} />
+        <button onClick={() => this.loginRequest(this.name, this.password)}>Hello</button>
       </div>
     )
   }
